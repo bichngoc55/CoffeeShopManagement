5d950c0b5aeafc0e0ae026f2b9c4b28c12dc94a2
Gấu <115764988+bichngoc55@users.noreply.github.com>
2024-04-16 19:04:58 +0700

    new commit for dashBoard and some padding

--- coffeeshop/src/App.js
+++ coffeeshop/src/App.js
@@ -6,44 +6,36 @@ import { createTheme } from "@mui/material/styles";
 import { useSelector } from "react-redux";
 import { useMemo } from "react";
 import LoginPage from "./pages/Login/Login";
-import DashBoard from "./components/dashBoard/dashBoard";
+import Booking from "./pages/BookingPage/booking";
 import { useState } from "react";
 import Home from "./pages/HomePage/home";
-import Menu from "./pages/MenuPage/menu";
+//import Menu from "./pages/MenuPage/menu";
 import Inventory from "./pages/InventoryPage/inventory";
 
 function App() {
   const mode = useSelector((state) => state.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   //const isAuth = Boolean(useSelector((state) => state.token));
-  const [isSidebar, setIsSidebar] = useState(true);
   return (
     <div className="App">
       <BrowserRouter>
         <ThemeProvider theme={theme}>
           <CssBaseline />
-          <div className="sidebar">
-            <DashBoard isSidebar={isSidebar} />
-          </div>
-          {/* <NavBar setIsSidebar={setIsSidebar} /> */}
-          <div className="main-content">
-            <Routes>
-              <Route path="/login" element={<LoginPage />} />
-              <Route path="/home" element={<Home />} />
-              <Route path="/inventory" element={<Inventory />} />
-              <Route path="/menu" element={<Menu />} />
-              {/* <Route
+          <Routes>
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/inventory" element={<Inventory />} />
+            {/* <Route path="/menu" element={<Menu />} /> */}
+            <Route path="/booking" element={<Booking />} />
+            {/* <Route
               path="/order"
               element={isAuth ? <OrderPage /> : <Navigate to="/" />}
             />  */}
-            </Routes>
-          </div>
+          </Routes>
         </ThemeProvider>
       </BrowserRouter>
     </div>
-
   );
 }
-
 
 export default App;
