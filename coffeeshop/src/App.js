c9e678c8a5661a00825f592be425ced637a0ed2a
tranuyn <127413543+tranuyn@users.noreply.github.com>
2024-04-18 00:13:35 +0700

    change path HomePage in App.js

--- coffeeshop/src/App.js
+++ coffeeshop/src/App.js
@@ -10,7 +10,7 @@ import LoginPage from "./pages/LoginPage/Login";
 import Booking from "./pages/BookingPage/booking";
 import Stuff from "./pages/StuffPage/stuff";
 import { useState } from "react";
-import Home from "./pages/home/HomePage";
+import Home from "./pages/HomePage/home";
 //import Menu from "./pages/MenuPage/menu";
 import Inventory from "./pages/InventoryPage/inventory";
 
