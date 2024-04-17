d2c5dd5b69aca6d3eafe876b0dc690cff79bb057
Hồ Kim Thiên Nga <127382679+cabungbu@users.noreply.github.com>
2024-04-17 18:04:21 +0700

    Up

--- coffeeshop/src/pages/StuffPage/stuff.jsx
+++ coffeeshop/src/pages/StuffPage/stuff.jsx
@@ -1,9 +1,32 @@
 import React from "react";
 import DashBoard from "../../components/dashBoard/dashBoard";
 import TabStaff from "../../components/TabStaff/TabStaff";
+import UserCard from "../../components/userCard/userCard";
 import "./stuff.css";
 import { Box } from "@mui/material";
 const Stuff = () => {
+  const items = [
+    {
+      title: "Employee Profile",
+      content: (
+        <div>
+          <div>
+            <UserCard />
+          </div>
+        </div>
+      ),
+    },
+    {
+      title: "Add Employee",
+      content: (
+        <div>
+          <p>
+            <strong>Add Employee 1</strong>
+          </p>
+        </div>
+      ),
+    },
+  ];
   return (
     <Box sx={{ display: "flex" }}>
       <DashBoard />
@@ -18,53 +41,3 @@ const Stuff = () => {
 };
 
 export default Stuff;
-
-const items = [
-  {
-    title: "Employee Profile",
-    content: (
-      <div>
-        <div>
-          <div>Employee Profile 1</div>
-          <div>Employee Profile 1</div>
-          <div>Employee Profile 1</div>
-          <div>Employee Profile 1</div>
-          <div>Employee Profile 1</div>
-          <div>Employee Profile 1</div>
-          <div>Employee Profile 1</div>
-          <div>Employee Profile 1</div>
-          <div>Employee Profile 1</div>
-          <div>Employee Profile 1</div>
-          <div>Employee Profile 1</div>
-          <div>Employee Profile 1</div>
-          <div>Employee Profile 1</div>
-          <div>Employee Profile 1</div>
-          <div>Employee Profile 1</div>
-          <div>Employee Profile 1</div>
-          <div>Employee Profile 1</div>
-          <div>Employee Profile 1</div>
-          <div>Employee Profile 1</div>
-          <div>Employee Profile 1</div>
-          <div>Employee Profile 1</div>
-          <div>Employee Profile 1</div>
-          <div>Employee Profile 1</div>
-          <div>Employee Profile 1</div>
-          <div>Employee Profile 1</div>
-          <div>Employee Profile 1</div>
-          <div>Employee Profile 1</div>
-          <div>Employee Profile 1</div>
-        </div>
-      </div>
-    ),
-  },
-  {
-    title: "Add Employee",
-    content: (
-      <div>
-        <p>
-          <strong>Add Employee 1</strong>
-        </p>
-      </div>
-    ),
-  },
-];
