fb91f2eace8c299d85f9f7ae8acc6d66b106febf
Hồ Kim Thiên Nga <127382679+cabungbu@users.noreply.github.com>
2024-05-01 23:52:03 +0700

    edit table done

--- coffeeshop/src/components/table/table.jsx
+++ coffeeshop/src/components/table/table.jsx
@@ -8,8 +8,74 @@ import PhoneAndroidIcon from "@mui/icons-material/PhoneAndroid";
 import GroupAddIcon from "@mui/icons-material/GroupAdd";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import EditNoteIcon from "@mui/icons-material/EditNote";
+<<<<<<< Updated upstream:coffeeshop/src/components/table/table.jsx
 import Gachchan from "./gachchan";
 const table = () => {
+=======
+import FormTable from "./modalTable";
+import { useSelector } from "react-redux";
+import Gachchan from "./gachchan";
+
+const TableInfo = ({ selectedTable }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openModal = () => {
+    setIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+  const { token } = useSelector((state) => state.auths);
+  const [table, setTable] = useState({
+    customerName: "",
+    tableNumber: 0,
+    bookingDate: new Date().toISOString(),
+    bookingTime: "",
+    numberOfPeople: 0,
+    phoneNumberBooking: "",
+    note: "",
+    status: "",
+  });
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        console.log("Fetching data table");
+        const response = await fetch(
+          `http://localhost:3005/booking/${selectedTable}`,
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+
+        if (response.ok) {
+          const data = await response.json();
+          console.log(data);
+          setTable({
+            customerName: data.customerName,
+            tableNumber: data.tableNumber,
+            bookingDate: data.bookingDate,
+            bookingTime: data.bookingTime,
+            numberOfPeople: data.numberOfPeople,
+            phoneNumberBooking: data.phoneNumberBooking,
+            note: data.note,
+            status: data.status,
+          });
+        } else {
+          console.error("Request failed with status:", response.status);
+        }
+      } catch (error) {
+        console.error("Request failed with error:", error);
+      }
+    };
+
+    fetchData();
+  }, [selectedTable]);
+>>>>>>> Stashed changes:coffeeshop/src/components/table/TableInfo.jsx
   return (
     <div className="tableInfoContainer">
       <h1 className="headerTable">Thông tin đặt bàn</h1>
@@ -19,7 +85,7 @@ const table = () => {
           <AccountCircle />
           <label className="labelCustomer">Khách hàng</label>
         </div>
-        <label>Lisa</label>
+        <label>{table.customerName}</label>
       </div>
       <div className="customerContainer" style={{ marginBottom: "20px" }}>
         <Gachchan />
@@ -30,7 +96,7 @@ const table = () => {
           <TableRestaurantIcon />
           <label className="labelCustomer">Mã bàn</label>
         </div>
-        <label>11</label>
+        <label>{table.tableNumber}</label>
       </div>
       <div className="customerContainer" style={{ marginBottom: "20px" }}>
         <Gachchan />
@@ -41,7 +107,7 @@ const table = () => {
           <CalendarMonthIcon />
           <label className="labelCustomer">Ngày đặt</label>
         </div>
-        <label>10/10/2024</label>
+        <label>{table.bookingDate}</label>
       </div>
       <div className="customerContainer" style={{ marginBottom: "20px" }}>
         <Gachchan />
@@ -52,7 +118,7 @@ const table = () => {
           <AccessTimeIcon />
           <label className="labelCustomer">Giờ đặt</label>
         </div>
-        <label>11:20</label>
+        <label>{table.bookingTime}</label>
       </div>
       <div className="customerContainer" style={{ marginBottom: "20px" }}>
         <Gachchan />
@@ -63,7 +129,7 @@ const table = () => {
           <PhoneAndroidIcon />
           <label className="labelCustomer">SĐT</label>
         </div>
-        <label>012345677</label>
+        <label>{table.phoneNumberBooking}</label>
       </div>
       <div className="customerContainer" style={{ marginBottom: "20px" }}>
         <Gachchan />
@@ -74,7 +140,7 @@ const table = () => {
           <GroupAddIcon />
           <label className="labelCustomer">Số lượng khách</label>
         </div>
-        <label>4</label>
+        <label>{table.numberOfPeople}</label>
       </div>
       <div className="customerContainer" style={{ marginBottom: "20px" }}>
         <Gachchan />
@@ -85,7 +151,7 @@ const table = () => {
           <MoreVertIcon />
           <label className="labelCustomer">Trạng thái</label>
         </div>
-        <label>Đã đặt trước</label>
+        <label>{table.status}</label>
       </div>
       <div className="customerContainer" style={{ marginBottom: "20px" }}>
         <Gachchan />
@@ -96,11 +162,18 @@ const table = () => {
           <EditNoteIcon />
           <label className="labelCustomer">Ghi chú</label>
         </div>
-        <label>none</label>
+        <label>{table.note}</label>
       </div>
       <div className="customerContainer" style={{ marginBottom: "20px" }}></div>
 
+<<<<<<< Updated upstream:coffeeshop/src/components/table/table.jsx
       <button className="btnE">Sửa</button>
+=======
+      <button className="btnE" onClick={openModal}>
+        Sửa
+      </button>
+      <FormTable isOpen={isOpen} onClose={closeModal} id={selectedTable} />
+>>>>>>> Stashed changes:coffeeshop/src/components/table/TableInfo.jsx
     </div>
   );
 };
