5b9631f9e49d7b67f6f5bd0619f8b531c3e3b061
tranuyn <127413543+tranuyn@users.noreply.github.com>
2024-05-07 09:50:52 +0700

    update HomePage, fix Analytics and Inventory

--- coffeeshop/src/pages/InventoryPage/inventory.jsx
+++ coffeeshop/src/pages/InventoryPage/inventory.jsx
@@ -5,7 +5,11 @@ const Inventory = () => {
   return (
     <Box sx={{ display: "flex" }}>
       <DashBoard />
-      <div style={{ padding: "16px" }}>Inventory</div>
+      <div className="container">
+        <div className="namePage font-semibold medium_text" style={{ marginTop: "2.15%", textAlign: "left" }}>
+          <a>Inventory</a>
+        </div>
+      </div>
     </Box>
   );
 };
