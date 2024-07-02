8622431fe38d819c67f742a854e7750ca45c8a54
Hồ Kim Thiên Nga <127382679+cabungbu@users.noreply.github.com>
2024-07-02 22:12:26 +0700

    update booking

--- coffeeshop/src/pages/BookingPage/booking.jsx
+++ coffeeshop/src/pages/BookingPage/booking.jsx
@@ -7,6 +7,7 @@ import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
 import TableRestaurantIcon from "@mui/icons-material/TableRestaurant";
 import { useSelector } from "react-redux";
 import TableInfo from "../../components/table/TableInfo";
+import Button from "@mui/material/Button";
 import "./booking.css";
 
 const Booking = () => {
@@ -14,43 +15,72 @@ const Booking = () => {
   const [availableTables, setAvailableTables] = useState(0);
   const [occupiedTables, setOccupiedTables] = useState(0);
   const [bookedTables, setBookedTables] = useState(0);
-
   const [selectedTable, setSelectedTable] = useState(
     "661ffb050f8b90fbff1b40ce"
   );
 
   const { token } = useSelector((state) => state.auths);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("http://localhost:3005/booking/", {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`,
-          },
-        });
-
-        if (response.ok) {
-          const data = await response.json();
-          setTable(data);
-          const statusCount = data.reduce((count, item) => {
-            count[item.status] = (count[item.status] || 0) + 1;
-            return count;
-          }, {});
-
-          setAvailableTables(statusCount.available || 0);
-          setOccupiedTables(statusCount.occupied || 0);
-          setBookedTables(statusCount.booked || 0);
-        } else {
-          console.error("Request failed with status:", response.status);
-        }
-      } catch (error) {
-        console.error("Request failed with error:", error);
-      }
+  const AddTable = async () => {
+    const newTable = {
+      tableNumber: table.length + 1,
+      status: "available",
     };
+    const response = await fetch("http://localhost:3005/booking/add", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newTable),
+    });
+    if (!response.ok) {
+      throw new Error(`Error add table: ${response}`);
+    } else fetchData();
+  };
 
+  const DeleteTable = async (id) => {
+    const response = await fetch(`http://localhost:3005/booking/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    if (!response.ok) {
+      throw new Error(`Error delete table: ${response}`);
+    } else fetchData();
+  };
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch("http://localhost:3005/booking/", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        setTable(data);
+        const statusCount = data.reduce((count, item) => {
+          count[item.status] = (count[item.status] || 0) + 1;
+          return count;
+        }, {});
+
+        setAvailableTables(statusCount.available || 0);
+        setOccupiedTables(statusCount.occupied || 0);
+        setBookedTables(statusCount.booked || 0);
+      } else {
+        console.error("Request failed with status:", response.status);
+      }
+    } catch (error) {
+      console.error("Request failed with error:", error);
+    }
+  };
+
+  useEffect(() => {
     fetchData();
   }, []);
 
@@ -118,6 +148,47 @@ const Booking = () => {
               );
             })}
           </div>
+        </div>
+        <div
+          style={{
+            width: "100%",
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            paddingLeft: "40px",
+            paddingRight: "40px",
+            marginTop: "30px",
+            marginBottom: "30px",
+          }}
+        >
+          <Button
+            sx={{
+              color: "#704332",
+              fontSize: 14,
+              fontWeight: 600,
+              width: "30%",
+              borderRadius: "20px",
+              borderColor: "#704332",
+            }}
+            variant="outlined"
+            onClick={() => AddTable()}
+          >
+            Add Table
+          </Button>
+          <Button
+            sx={{
+              color: "#704332",
+              fontSize: 14,
+              fontWeight: 600,
+              width: "30%",
+              borderRadius: "20px",
+              borderColor: "#704332",
+            }}
+            variant="outlined"
+            onClick={() => DeleteTable(table[table.length - 1]._id)}
+          >
+            Delete Table
+          </Button>
         </div>
       </div>
 
